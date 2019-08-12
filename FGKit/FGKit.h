bb33254673fe00e12a7843ab4294546b11e87c39
Gabriel Gárate Vivanco <gabriel.garate@ionix.cl>
2019-08-12 17:35:53 -0400

    [FGKit-SN] Se puso de manera publica libreria RNOpenSSLDecryptor.

--- FGKit/FGKit.h
+++ FGKit/FGKit.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char FGKitVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <FGKit/PublicHeader.h>
 
-
+#import "RNOpenSSLDecryptor.h"
