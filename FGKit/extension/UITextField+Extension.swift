112a98355740fa0edcd42c89e3926c9e84d3ff6b
gabriel <hola@gabrielgv.com>
2019-09-05 16:18:56 -0400

    [FGKit-SN] Se agrega la primera instancia de errorview. [FGKit-SN] Se agregan elementos para trabajar con UITextField.

---
+++ FGKit/extension/UITextField+Extension.swift
@@ -0,0 +1,35 @@
+//
+//  UITextField+Extension.swift
+//  FGKit
+//
+//  Created by Gabriel Gárate Vivanco on 9/4/19.
+//  Copyright © 2019 weeKG. All rights reserved.
+//
+
+public extension UITextField {
+    
+    @IBInspectable var doneAccessory: Bool {
+        get {
+            return self.doneAccessory
+        }
+        set (hasDone) {
+            if hasDone {
+                addDoneButtonOnKeyboard()
+            }
+        }
+    }
+    
+    func addDoneButtonOnKeyboard() {
+        let doneToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        doneToolbar.barStyle = .default
+        doneToolbar.sizeToFit()
+        doneToolbar.items = [
+            UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil),
+            UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(self.doneButtonAction))
+        ]
+        self.inputAccessoryView = doneToolbar
+    }
+    @objc func doneButtonAction() {
+        self.resignFirstResponder()
+    }
+}
