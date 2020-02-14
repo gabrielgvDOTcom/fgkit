15cf74a6c282dfabe18cbbd51a8609faa6716c0d
Gabriel Gárate Vivanco <hola@gabrielgv.com>
2020-02-14 09:29:06 -0300

    [FGKit-SN] Se agrega color al texto de botones.

--- FGKit/styled/extension/UIButton+Extension.swift
+++ FGKit/styled/extension/UIButton+Extension.swift
@@ -9,11 +9,12 @@
 import Foundation
 
 public extension UIButton {
-    
+
     func style(_ style: Styled.Button) {
         self.backgroundColor = style.bgColor
         self.titleLabel?.font = style.font
         self.tintColor = style.tintColor
+        self.setTitleColor(style.tintColor, for: .normal)
         self.layer.cornerRadius = style.cornerRadius
         self.layer.masksToBounds = true
     }
