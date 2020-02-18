1e85e67f03d1f26a68eb96a21f53bf17ebbbb335
Gabriel Gárate Vivanco <hola@gabrielgv.com>
2020-02-18 14:45:30 -0300

    [FGKit-SN] Se corrige pequeño error.

--- FGKit/extension/Bundle+Extension.swift
+++ FGKit/extension/Bundle+Extension.swift
@@ -1,16 +1,16 @@
 //
-//  Bundle+Extensión.swift
+//  Bundle+Extension.swift
 //  FGKit
 //
 //  Created by Gabriel Gárate vivanco on 18-02-20.
 //  Copyright © 2020 weeKG. All rights reserved.
 //
 
+private class __ {}
+
 extension Bundle {
 
     static var current: Bundle {
-
-        class __ { }
         return Bundle(for: __.self)
     }
 }
